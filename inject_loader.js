d5d7d141eaa274b0dccce6fce5ebe4964364ba11
Oshinpou <oshinpounikar@gmail.com>
2025-03-28 17:27:20 +0530

    Create inject_loader.js

---
+++ inject_loader.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+    var script = document.createElement('script');
+    script.src = "https://your-github-link/wasm/inject.js"; // Replace with your GitHub link
+    document.body.appendChild(script);
+})();
