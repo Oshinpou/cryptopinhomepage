88b420bdbb5819954978eea0083411b56c98b1db
Oshinpou <oshinpounikar@gmail.com>
2025-03-28 17:09:59 +0530

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+    var script = document.createElement('script');
+    script.src = "https://your-github-link/inject/inject.js";
+    document.body.appendChild(script);
+})();
