16720245b37d475af19a212d6d8cb17a738b587c
Oshinpou <oshinpounikar@gmail.com>
2025-04-15 07:38:19 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -8,3 +8,14 @@ function paymentSuccess(amount) {
     backgroundColor: "green",
   }).showToast();
 }
+
+function showError(message) {
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: "top", 
+    position: "center", 
+    backgroundColor: "red",
+  }).showToast();
+}
